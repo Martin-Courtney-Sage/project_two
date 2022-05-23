7f26cce37f4447e2b82d3f0cca57185a091a51bd
Naomi Scott <33094951+naelscot@users.noreply.github.com>
2022-05-23 12:00:49 -0400

    Add files via upload

---
+++ ProjectTwo(Python)/models/users.py
@@ -0,0 +1,25 @@
+class Users:
+    def __init__(self, user_id=0, first_name='', last_name='', email='', phone=''):
+        self.user_id = user_id,
+        self.first_name = first_name,
+        self.last_name = last_name,
+        self.email = email,
+        self.phone = phone
+
+    def __repr__(self):
+        return str ({
+            'user_id' : self.user_id,
+            'first_name' : self.first_name,
+            'last_name' : self.last_name,
+            'email' : self.email,
+            'phone' : self.phone
+        })
+
+    def json(self):
+        return str ({
+            'userId' : self.user_id,
+            'firstName' : self.first_name,
+            'lastName' : self.last_name,
+            'email' : self.email,
+            'phone' : self.phone
+        })
